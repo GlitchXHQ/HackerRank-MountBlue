27b8eb5d35cdf9095def73a854a08f5d9dd97997
GlitchXHQ <er.vivekchauhan1@gmail.com>
2026-08-14 23:08:47 +0530

    Questions Done

---
+++ 36.Marc'sCakewalk.cpp
@@ -0,0 +1,149 @@
+/*
+Marc loves cupcakes, but he also likes to stay fit. Each cupcake has a calorie count, and Marc can walk a distance to expend those calories. If Marc has eaten  cupcakes so far, after eating a cupcake with  calories he must walk at least  miles to maintain his weight.
+
+Example
+
+If he eats the cupcakes in the order shown, the miles he will need to walk are . This is not the minimum, though, so we need to test other orders of consumption. In this case, our minimum miles is calculated as .
+
+Given the individual calorie counts for each of the cupcakes, determine the minimum number of miles Marc must walk to maintain his weight. Note that he can eat the cupcakes in any order.
+
+Function Description
+
+Complete the marcsCakewalk function in the editor below.
+
+marcsCakewalk has the following parameter(s):
+
+int calorie[n]: the calorie counts for each cupcake
+Returns
+
+long: the minimum miles necessary
+Input Format
+
+The first line contains an integer , the number of cupcakes in .
+The second line contains  space-separated integers, .
+
+Constraints
+
+Sample Input 0
+
+3
+1 3 2
+Sample Output 0
+
+11
+Explanation 0
+
+Let's say the number of miles Marc must walk to maintain his weight is . He can minimize  by eating the  cupcakes in the following order:
+
+Eat the cupcake with  calories, so .
+Eat the cupcake with  calories, so .
+Eat the cupcake with  calories, so .
+We then print the final value of , which is , as our answer.
+
+Sample Input 1
+
+4
+7 4 9 6
+Sample Output 1
+
+79
+Explanation 1
+(2° *9) + (2^1 * 7) + (2^2 *6) + (2^3 * 4) = 9 + 14 + 24 + 32 = 79
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'marcsCakewalk' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY calorie as parameter.
+ */
+
+long marcsCakewalk(vector<int> calorie) {
+    sort(calorie.begin(),calorie.end(),greater<int>());
+    
+    long ans=0;
+
+    for(int i=0; i<calorie.size() ; i++){
+        ans+=pow(2,i) * calorie[i];
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string calorie_temp_temp;
+    getline(cin, calorie_temp_temp);
+
+    vector<string> calorie_temp = split(rtrim(calorie_temp_temp));
+
+    vector<int> calorie(n);
+
+    for (int i = 0; i < n; i++) {
+        int calorie_item = stoi(calorie_temp[i]);
+
+        calorie[i] = calorie_item;
+    }
+
+    long result = marcsCakewalk(calorie);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
